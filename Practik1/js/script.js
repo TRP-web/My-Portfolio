917bdad9f7571477952520bafbc16214dd6a1363
TRP-web <79875751+TRP-web@users.noreply.github.com>
2021-10-03 12:36:09 +0300

    Add files via upload

---
+++ Practik1/js/script.js
@@ -0,0 +1,26 @@
+const img = document.querySelector('.article__img');
+
+img .addEventListener('click', (event) =>{
+  img.classList.toggle('active')
+})
+
+const menuLinks = document.querySelectorAll('.article-footer__link[data-goto]');
+
+if (menuLinks.length > 0) {
+  menuLinks.forEach(elem => {
+    elem.addEventListener("click", onClick);
+  });
+  function onClick(elem) {
+    const menuLink = elem.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      elem.preventDefault();
+    }
+  }
+}
